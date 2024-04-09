6389e07e1982416e44cc4f480599ca439124c8c0
Vishal Kumar <vishalkumar61952@gmail.com>
2024-04-09 16:37:57 +0530

    Collecting & sending login & signup data to backend and added password hide/visible functionality in login & signup form

--- client/src/components/LoginSignup/Login.jsx
+++ client/src/components/LoginSignup/Login.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { IoMdEyeOff } from "react-icons/io";
+import { IoEye } from "react-icons/io5";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function changeHandler(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  async function submitHandler(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Form submitted successfully");
+        toast.success("Logged in");
+        setFormData({ email: "", password: "" });
+      } else {
+        toast.error("Form submission failed");
+        console.error("Form submission failed");
+      }
+    } catch (error) {
+      toast.error("Error submitting form");
+      console.error("Error submitting form:", error);
+    }
+  }
+
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-300">
       <div className="w-96 h-2/3 bg-rose-100 rounded-2xl flex flex-col items-center justify-evenly">
@@ -8,28 +52,48 @@ const Login = () => {
           Welcome to CodeHub!
         </h1>
 
-        <form action="" className="flex flex-col gap-y-4">
+        <form onSubmit={submitHandler} className="flex flex-col gap-y-4">
           <div className="flex flex-col gap-y-1">
-            <label htmlFor="username" className="font-medium">
-              Username
+            <label htmlFor="email" className="font-medium">
+              Email
             </label>
             <input
               type="text"
-              id="username"
+              id="email"
               className="rounded-full pl-3 py-2 focus:outline-double"
-              placeholder="Enter your username"
+              placeholder="Enter your email"
+              name="email"
+              onChange={changeHandler}
+              value={formData.email}
             />
           </div>
-          <div className="flex flex-col gap-y-1">
+          <div className="flex flex-col gap-y-1 relative">
             <label htmlFor="password" className="font-medium">
               Password
             </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               id="password"
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Enter your password"
+              name="password"
+              onChange={changeHandler}
+              value={formData.password}
             />
+            <span
+              onClick={() => {
+                setShowPassword((prev) => {
+                  setShowPassword(!prev);
+                });
+              }}
+              className="absolute top-9 right-3 cursor-pointer"
+            >
+              {showPassword ? (
+                <IoEye fontSize={23} />
+              ) : (
+                <IoMdEyeOff fontSize={23} />
+              )}
+            </span>
           </div>
           <button
             type="submit"
@@ -44,9 +108,9 @@ const Login = () => {
           </div>
           <div className="flex gap-x-3">
             <p>Don't have an account?</p>
-            <a href="" className="text-blue-500 hover:text-blue-700">
+            <Link to="/signup" className="text-blue-500 hover:text-blue-700">
               Sign Up
-            </a>
+            </Link>
           </div>
         </form>
       </div>
--- client/src/components/LoginSignup/Signup.jsx
+++ client/src/components/LoginSignup/Signup.jsx
@@ -1,6 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { IoMdEyeOff } from "react-icons/io";
+import { IoEye } from "react-icons/io5";
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfPassword, setShowConfPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  });
+
+  function changeHandler(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  async function submitHandler(event) {
+    event.preventDefault();
+    if (formData.password != formData.confPassword) {
+      toast.error("Passwords don't match");
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Form submitted successfully");
+        toast.success("Account Created");
+        setFormData({
+          username: "",
+          email: "",
+          password: "",
+          confPassword: "",
+        });
+      } else {
+        toast.error("Form submission failed");
+        console.error("Form submission failed");
+      }
+    } catch (error) {
+      toast.error("Error submitting form");
+      console.error("Error submitting form:", error);
+    }
+  }
+
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-300">
       <div className="w-96 h-4/5 bg-rose-100 rounded-2xl flex flex-col items-center justify-evenly">
@@ -8,7 +64,7 @@ const Signup = () => {
           Welcome to CodeHub!
         </h1>
 
-        <form action="" className="flex flex-col gap-y-4">
+        <form onSubmit={submitHandler} className="flex flex-col gap-y-4">
           <div className="flex flex-col gap-y-1">
             <label htmlFor="username" className="font-medium">
               Username
@@ -18,6 +74,9 @@ const Signup = () => {
               id="username"
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Create your username"
+              name="username"
+              onChange={changeHandler}
+              value={formData.username}
             />
           </div>
           <div className="flex flex-col gap-y-1">
@@ -29,29 +88,66 @@ const Signup = () => {
               id="email"
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Enter your email"
+              name="email"
+              onChange={changeHandler}
+              value={formData.email}
             />
           </div>
-          <div className="flex flex-col gap-y-1">
+          <div className="flex flex-col gap-y-1 relative">
             <label htmlFor="password" className="font-medium">
               Password
             </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               id="password"
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Create your password"
+              name="password"
+              onChange={changeHandler}
+              value={formData.password}
             />
+            <span
+              onClick={() => {
+                setShowPassword((prev) => {
+                  setShowPassword(!prev);
+                });
+              }}
+              className="absolute top-9 right-3 cursor-pointer"
+            >
+              {showPassword ? (
+                <IoEye fontSize={23} />
+              ) : (
+                <IoMdEyeOff fontSize={23} />
+              )}
+            </span>
           </div>
-          <div className="flex flex-col gap-y-1">
-            <label htmlFor="confirm-pass" className="font-medium">
+          <div className="flex flex-col gap-y-1 relative">
+            <label htmlFor="confPassword" className="font-medium">
               Confirm Password
             </label>
             <input
-              type="password"
-              id="confirm-pass"
+              type={showConfPassword ? "text" : "password"}
+              id="confPassword"
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Confirm Password"
+              name="confPassword"
+              onChange={changeHandler}
+              value={formData.confPassword}
             />
+            <span
+              onClick={() => {
+                setShowConfPassword((prev) => {
+                  setShowConfPassword(!prev);
+                });
+              }}
+              className="absolute top-9 right-3 cursor-pointer"
+            >
+              {showConfPassword ? (
+                <IoEye fontSize={23} />
+              ) : (
+                <IoMdEyeOff fontSize={23} />
+              )}
+            </span>
           </div>
           <button
             type="submit"
@@ -61,9 +157,9 @@ const Signup = () => {
           </button>
           <div className="flex gap-x-3">
             <p>Already have an account?</p>
-            <a href="" className="text-blue-500 hover:text-blue-700">
+            <Link to="/login" className="text-blue-500 hover:text-blue-700">
               Login
-            </a>
+            </Link>
           </div>
         </form>
       </div>
