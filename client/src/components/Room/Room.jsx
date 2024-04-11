0d9ef50757cc0fb5b53ea2cf6585cf079112ef07
Vishal21121 <vs84077000@gmail.com>
2024-04-11 20:29:28 +0530

    added: code for fetching the code,board, and chats

--- client/src/components/Room/Room.jsx
+++ client/src/components/Room/Room.jsx
@@ -1,11 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useDebugValue, useEffect, useState } from 'react'
 import Monaco from '../CodeEditor/Monaco'
 import { useRoomContext } from '../../context/room/RoomContext'
 import { Whiteboard } from '../WhiteBoard/Whiteboard'
 import ChatBox from '../Chatbox/ChatBox'
+import { useParams } from "react-router-dom"
 
 function Room() {
-    const { roomData, mode } = useRoomContext()
+    const { mode, setBoardData, setEditorData, setChatsData } = useRoomContext()
+    const { roomId } = useParams()
+
+    const fetchData = async (roomId) => {
+        try {
+            const [codeResonse, boardResponse, chatResponse] = await Promise.all([
+                fetch(`http://localhost:5000/api/code/get/${roomId}`),
+                fetch(`http://localhost:5000/api/board/get/${roomId}`),
+                fetch(`http://localhost:5000/api/chat/get/${roomId}`),
+            ]);
+
+            const codeReceived = await codeResonse.json();
+            const boardReceived = await boardResponse.json();
+            const chatReceived = await chatResponse.json();
+
+            if (codeReceived.data.statusCode === 200) {
+                let html = codeReceived.data.value.html
+                let css = codeReceived.data.value.css
+                let js = codeReceived.data.value.js
+                let _id = codeReceived.data.value._id
+                setEditorData({ html, css, js, _id })
+            }
+            if (boardReceived.data.statusCode === 200) {
+                let content = JSON.parse(boardReceived.data.value.content)
+                let _id = boardReceived.data.value._id
+                setBoardData({ content, _id })
+            }
+            if (chatReceived.data.statusCode === 200) {
+                setChatsData(chatReceived.data.value)
+            }
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+
+    useEffect(() => {
+        fetchData(roomId)
+    }, [])
 
     return (
         <div>
