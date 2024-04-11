7bbf6df2eea81ff62ce292364436b00fa5d328a2
Vishal21121 <vs84077000@gmail.com>
2024-04-11 16:16:17 +0530

    added: code for conditional rendering of navbar elements

--- client/src/components/TopNavbar/TopNavbar.jsx
+++ client/src/components/TopNavbar/TopNavbar.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useUserContext } from "../../context/user/UserContext"
 
 const TopNavbar = () => {
+  const { userData } = useUserContext()
   return (
     <div>
-        <nav className="bg-gray-800 p-4">
+      <nav className="bg-gray-800 p-4">
         <div className="flex items-center justify-between">
           <Link to="/" className="text-white text-2xl font-bold">CodeHub</Link>
           <div className="flex space-x-10">
-            <Link to="/editor" className="text-white">
-              Open Editor
-            </Link>
-            <Link to="/chat" className="text-white">
-              Ask GPT 3.5
-            </Link>
-            <Link to="/whiteboard" className="text-white">
-              Whiteboard
-            </Link>
-            <Link to="/login" className="text-white">
-              Login
-            </Link>
-            <Link to="/signup" className="text-white">
-              Sign up
-            </Link>
+            {
+              userData ? (
+                <div className='flex space-x-10'>
+                  <Link to="/editor" className="text-white">
+                    Open Editor
+                  </Link>
+                  <Link to="/chat" className="text-white">
+                    Ask GPT 3.5
+                  </Link>
+                  <Link to="/whiteboard" className="text-white">
+                    Whiteboard
+                  </Link>
+                </div>
+              ) : <div className='flex space-x-10'>
+                <Link to="/login" className="text-white">
+                  Login
+                </Link>
+                <Link to="/signup" className="text-white">
+                  Sign up
+                </Link>
+              </div>
+            }
+
+
           </div>
         </div>
       </nav>
