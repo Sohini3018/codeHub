3e3403d725c0f1ee8d9f2a979f1d23d31e199c78
Vishal21121 <vs84077000@gmail.com>
2024-04-27 13:01:00 +0530

    added: use-hook-form in signup page

--- client/src/components/LoginSignup/Signup.jsx
+++ client/src/components/LoginSignup/Signup.jsx
@@ -3,52 +3,54 @@ import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { IoMdEyeOff } from "react-icons/io";
 import { IoEye } from "react-icons/io5";
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfPassword, setShowConfPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confPassword: "",
-  });
   const navigate = useNavigate()
 
-  function changeHandler(event) {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [name]: value,
-      };
-    });
-  }
+  const signupSchema = z.object({
+    username:z.string({
+      invalid_type_error:"Username must be of 3 characters",
+      required_error:"Username is required"
+    }).min(3),
+    email:z.string({
+      invalid_type_error:"Invalid email is provided",
+      required_error:"Email is required"
+    }).email(),
+    password: z.string({
+      required_error:"Password is required",
+      invalid_type_error:"Password must be of atleast 8 characters"
+    }).min(8),
+    confPassword: z.string({
+      required_error:"Password is required",
+      invalid_type_error:"Password must be of atleast 8 characters"
+    }).min(8)
+  }).refine(data => data.password === data.confPassword, {
+        message: "Passwords must match",
+        path: ["confPassword"]
+    })
 
-  async function submitHandler(event) {
-    event.preventDefault();
-    if (formData.password != formData.confPassword) {
-      toast.error("Passwords don't match");
-      return;
-    }
+  const {register,handleSubmit,formState:{errors}} = useForm({
+    resolver: zodResolver(signupSchema)
+  })
+
+  async function submitHandler(data) {
     try {
       const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(data),
       });
       if (response.ok) {
         console.log("Form submitted successfully");
         navigate("/roomJoin")
         toast.success("Account Created");
-        setFormData({
-          username: "",
-          email: "",
-          password: "",
-          confPassword: "",
-        });
       } else {
         toast.error("Form submission failed");
         console.error("Form submission failed");
@@ -61,12 +63,12 @@ const Signup = () => {
 
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-300">
-      <div className="w-96 h-4/5 bg-rose-100 rounded-2xl flex flex-col items-center justify-evenly">
+      <div className="w-96 h-[90%] bg-rose-100 rounded-2xl flex flex-col items-center justify-evenly">
         <h1 className="text-2xl font-bold tracking-tighter">
           Welcome to CodeHub!
         </h1>
 
-        <form onSubmit={submitHandler} className="flex flex-col gap-y-4">
+        <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col gap-y-4">
           <div className="flex flex-col gap-y-1">
             <label htmlFor="username" className="font-medium">
               Username
@@ -77,9 +79,9 @@ const Signup = () => {
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Create your username"
               name="username"
-              onChange={changeHandler}
-              value={formData.username}
+              {...register("username")}
             />
+            {errors.username && <div className="text-red-500 text-xs">{errors.username.message}</div>}
           </div>
           <div className="flex flex-col gap-y-1">
             <label htmlFor="email" className="font-medium">
@@ -91,9 +93,9 @@ const Signup = () => {
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Enter your email"
               name="email"
-              onChange={changeHandler}
-              value={formData.email}
+              {...register("email")}
             />
+            {errors.email && <div className="text-red-500 text-xs">{errors.email.message}</div>}
           </div>
           <div className="flex flex-col gap-y-1 relative">
             <label htmlFor="password" className="font-medium">
@@ -105,9 +107,9 @@ const Signup = () => {
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Create your password"
               name="password"
-              onChange={changeHandler}
-              value={formData.password}
+              {...register("password")}
             />
+            {errors.password && <div className="text-red-500 text-xs">{errors.password.message}</div>}
             <span
               onClick={() => {
                 setShowPassword((prev) => {
@@ -133,9 +135,9 @@ const Signup = () => {
               className="rounded-full pl-3 py-2 focus:outline-double"
               placeholder="Confirm Password"
               name="confPassword"
-              onChange={changeHandler}
-              value={formData.confPassword}
+             {...register("confPassword")}
             />
+            {errors.confPassword && <div className="text-red-500 text-xs">{errors.confPassword.message}</div>}
             <span
               onClick={() => {
                 setShowConfPassword((prev) => {
